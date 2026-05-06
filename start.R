cbc0a1ae9a98efecb92179993e15242d93c86add
GRΞG <1288655+gbg184@users.noreply.github.com>
2026-05-06 11:53:48 -0600

    Update start.R
    En R puedes editar una columna de varias formas, dependiendo de qué quieres hacer exactamente

--- start.R
+++ start.R
@@ -1,2 +1,11 @@
 Open <- read.csv('Clasificacion-Incidentes-2026-r.csv')
 summary(Open)
+
+#Si quieres editar manualmente
+fix(Open)
+
+#Si está vacío → poner ‘Medio’
+Open$Ambiente[Open$Ambiente == ""] <- "Medio"
+
+#Convertir tipo de columna
+Open$Fecha_Apertura <- as.Date(Open$Fecha_Apertura, format="%d/%m/%Y")
